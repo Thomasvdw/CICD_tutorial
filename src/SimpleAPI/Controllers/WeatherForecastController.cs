0923b3c0d09af2a042e2251ead497578b4a4d44a
Thomas van der Wardt <thomas_vdw@live.nl>
2020-10-21 13:15:07 +0200

    Change to controller to fail unit test

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -31,7 +31,7 @@ namespace SimpleAPI.Controllers
         //public IEnumerable<WeatherForecast> Get()
         public ActionResult<string> Get()
         {
-            return "Stuff";
+            return "Thomas!";
             /*
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
